0cce8dc1ee452772ad5eaa4eeadb7cc78a3e0896
João Daniel Silva <joao.daniel.silva@tecnico.ulisboa.pt>
2021-09-02 03:17:44 +0100

    supcon task

--- models/SupConLoss/supcon_utils.py
+++ models/SupConLoss/supcon_utils.py
@@ -9,6 +9,24 @@ https://github.com/HobbitLong/SupContrast
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from tqdm import tqdm
+import numpy as np
+import random
+from transformers import BertTokenizer, BertModel
+from googletrans import Translator
+import os
+from PIL import Image
+from roco_utils import encode_text
+from torch.utils.data import Dataset, DataLoader
+
+
+class TwoCropTransform:
+    """Create two crops of the same image"""
+    def __init__(self, transform):
+        self.transform = transform
+
+    def __call__(self, x):
+        return [self.transform(x), self.transform(x)]
 
 model_dict = {
     'resnet18':  512,
@@ -17,15 +35,20 @@ model_dict = {
     'resnet101': 2048,
     'resnet152': 2048,
     'efficientnet-b3': 1536,
-    'efficientnet-b5': 2048
+    'efficientnet-b5': 2048,
+    'tf_efficientnetv2_m': 1280
 }
 
 class SupConEncoder(nn.Module):
     """backbone + projection head"""
-    def __init__(self, encoder, name='resnet50', head='mlp', feat_dim=128):
+    def __init__(self, encoder, name='resnet152', head='mlp', feat_dim=128):
         super(SupConEncoder, self).__init__()
         dim_in = model_dict[name]
         self.encoder = encoder
+        self.name = name
+        self.gap = nn.AdaptiveAvgPool2d((1,1))
+        if 'resnet' in name:
+            self.encoder.fc = nn.Sequential()
         if head == 'linear':
             self.head = nn.Linear(dim_in, feat_dim)
         elif head == 'mlp':
@@ -39,6 +62,214 @@ class SupConEncoder(nn.Module):
                 'head not supported: {}'.format(head))
 
     def forward(self, x):
-        feat = self.encoder(x)
+        feat = self.features(x)
         feat = F.normalize(self.head(feat), dim=1)
         return feat
+
+    def features(self,x):
+        if 'resnet' in self.name:
+            return self.encoder(x)
+        elif 'efficientnetv2' in self.name:
+            return self.gap(self.encoder.forward_features(x)).squeeze()
+        elif 'efficientnet' in self.name:
+            return self.encoder.extract_features(x)
+
+def get_supcon_model(model, args):
+    return SupConEncoder(model.transformer.trans.model, name=args.cnn_encoder)
+
+def jaccard_similarity(doc1, doc2): 
+    
+    # List the unique words in a document
+    words_doc1 = set(doc1.lower().split()) 
+    words_doc2 = set(doc2.lower().split())
+    
+    # Find the intersection of words list of doc1 & doc2
+    intersection = words_doc1.intersection(words_doc2)
+
+    # Find the union of words list of doc1 & doc2
+    union = words_doc1.union(words_doc2)
+        
+    # Calculate Jaccard similarity score 
+    # using length of intersection set divided by length of union set
+    if len(union) != 0:
+        return float(len(intersection)) / len(union)
+    else:
+        print('union == 0\n1st: ', doc1,'\n2nd: ',doc2)
+        return 0.0
+
+def buildMask(bsz,caption, aug):
+    mask = torch.zeros(bsz, bsz, dtype=torch.float)
+    for c1 in range(len(caption)):
+        for c2 in range(len(aug)):
+            mask[c1,c2] = jaccard_similarity(caption[c1],aug[c2])
+    
+    return mask
+
+class ROCO_SupCon(Dataset):
+    def __init__(self, args, df, tfm, keys, mode):
+        self.df = df.values
+        self.args = args
+        self.path = args.data_dir
+        self.tfm = tfm
+        self.keys = keys
+        self.mode = mode
+
+        self.translator = Translator()
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+        self.clinicalbert = None
+        
+        
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        name = self.df[idx,1] 
+        path = os.path.join(self.path, self.mode, 'radiology', 'images',name)
+
+        img = Image.open(path).convert('RGB')
+        
+        if self.tfm:
+            img = self.tfm(img)
+    
+        caption = self.df[idx, 2].strip()       
+        tokens, segment_ids, input_mask, targets = encode_text(caption, self.tokenizer, self.keys, self.args, self.clinicalbert)
+
+        aug_caption = self.translate_caption(caption)
+        aug_tokens, _, _, aug_targets = encode_text(aug_caption, self.tokenizer, self.keys, self.args, self.clinicalbert)
+        
+
+        return img, tokens, aug_tokens, segment_ids, input_mask, targets, aug_targets, caption, aug_caption
+
+    def translate_caption(self, caption):
+        langs = ['fr','de','pt']
+        l = random.choice(langs)
+        result = self.translator.translate(caption, src='en', dest=l)
+        final = self.translator.translate(result.text, src=l, dest='en')
+        return final.text
+
+def process_tensors(img,caption_token,aug_tokens,segment_ids,attention_mask,target,aug_targets):
+    def cat_tensors(a,b):
+        return torch.cat([a,b], dim=0)
+    return cat_tensors(img[0],img[1]),cat_tensors(caption_token,aug_tokens),cat_tensors(segment_ids,segment_ids),cat_tensors(attention_mask,attention_mask),cat_tensors(target,aug_targets)
+
+def train_one_epoch(loader, model, supcon_model, criterion, supcon_loss, optimizer, device, args, epoch):
+
+    model.train()
+    train_loss = []
+    PREDS = []
+    TARGETS = []
+    bar = tqdm(loader, leave=False)
+    for i, (img, caption_token,aug_tokens,segment_ids,attention_mask,target,aug_targets,caption_text,aug_text) in enumerate(bar):
+        img,caption_token,segment_ids,attention_mask,target = process_tensors(img,caption_token,aug_tokens,segment_ids,attention_mask,target,aug_targets)
+        img, caption_token,segment_ids,attention_mask,target = img.to(device), caption_token.to(device), segment_ids.to(device), attention_mask.to(device), target.to(device)
+        
+        caption_token = caption_token.squeeze(1)
+        attention_mask = attention_mask.squeeze(1)
+    
+        loss_func = criterion
+        optimizer.zero_grad()
+
+        
+        logits = model(img, caption_token, segment_ids, attention_mask)
+        logits = logits.log_softmax(-1)  # (bs x seq_len x vocab_size)
+        loss = loss_func(logits.permute(0,2,1), target)  
+        
+        bsz = img.shape[0] //2
+        features = supcon_model(img) # (bs x feat_dim)
+        f1, f2 = torch.split(features, [bsz, bsz], dim=0) # (bs//2 x feat_dim), (bs//2 x feat_dim)
+        features = torch.cat([f1.unsqueeze(1), f2.unsqueeze(1)], dim=1) # (bs//2, 2, feat_dim)
+
+        mask = buildMask(bsz,caption_text, aug_text)
+        loss_supcon = supcon_loss(features, mask=mask)
+
+        loss = loss + loss_supcon
+
+        loss.backward()
+        optimizer.step()    
+
+        # print('e')
+        # import IPython; IPython.embed(); import sys; sys.exit(0)
+           
+    
+        bool_label = target > 0
+
+        pred = logits[bool_label, :].argmax(1)
+        valid_labels = target[bool_label]   
+        
+        PREDS.append(pred)
+        TARGETS.append(valid_labels)
+        
+        acc = (pred == valid_labels).type(torch.float).mean() * 100.
+
+        loss_np = loss.detach().cpu().numpy()
+        train_loss.append(loss_np)
+        
+        
+        bar.set_description('train_loss: %.5f, train_acc: %.2f' % (loss_np, acc))
+        
+
+        
+    PREDS = torch.cat(PREDS).cpu().numpy()
+    TARGETS = torch.cat(TARGETS).cpu().numpy()
+
+#     # Calculate total accuracy
+    total_acc = (PREDS == TARGETS).mean() * 100.
+
+
+    return np.mean(train_loss), total_acc
+
+'''delete this vvvv'''
+def validate(loader, model, supcon_model,criterion, supcon_loss, scaler, device, args, epoch):
+
+    model.eval()
+    val_loss = []
+
+    PREDS = []
+    TARGETS = []
+    bar = tqdm(loader, leave=False)
+
+    with torch.no_grad():
+        for i, (img, caption_token,segment_ids,attention_mask,target) in enumerate(bar):
+
+            img, caption_token,segment_ids,attention_mask,target = img.to(device), caption_token.to(device), segment_ids.to(device), attention_mask.to(device), target.to(device)
+            caption_token = caption_token.squeeze(1)
+            attention_mask = attention_mask.squeeze(1)
+            
+            loss_func = criterion
+
+            
+            logits = model(img, caption_token, segment_ids, attention_mask)
+            
+            logits = logits.log_softmax(-1)  # (bs x seq_len x vocab_size)
+            loss = loss_func(logits.permute(0,2,1), target)
+                    
+
+            
+            bool_label = target > 0
+            pred = logits[bool_label, :].argmax(1)
+            valid_labels = target[bool_label]   
+        
+            PREDS.append(pred)
+            TARGETS.append(valid_labels)
+            
+            acc = (pred == valid_labels).type(torch.float).mean() * 100.
+
+            loss_np = loss.detach().cpu().numpy()
+
+            val_loss.append(loss_np)
+
+            bar.set_description('val_loss: %.5f, val_acc: %.5f' % (loss_np, acc))
+           
+
+        val_loss = np.mean(val_loss)
+
+    
+    PREDS = torch.cat(PREDS).cpu().numpy()
+    TARGETS = torch.cat(TARGETS).cpu().numpy()
+
+    # Calculate total accuracy
+    total_acc = (PREDS == TARGETS).mean() * 100.
+
+    return val_loss, PREDS, total_acc
+'''delete this ^^^^'''
---
+++ pretrain/roco_supcon_train.py
@@ -0,0 +1,179 @@
+import os
+import wandb
+import argparse
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.cuda.amp import GradScaler
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch import optim
+from torch.optim import lr_scheduler
+
+from roco_utils import load_mlm_data,get_keywords, ROCO, validate
+
+from models.mmbert import Model, get_transformer_model
+
+from models.SupConLoss.supcon_utils import ROCO_SupCon, train_one_epoch, get_supcon_model, TwoCropTransform
+from models.SupConLoss.loss import SupConLoss
+
+if __name__ == '__main__':
+    __spec__ = None
+    parser = argparse.ArgumentParser(description="Pretrain on ROCO with MLM")
+
+    parser.add_argument('-r', '--run_name', type=str, help="name for wandb run", required=True)
+    parser.add_argument('--data_dir', type=str, default = 'roco', help='path to dataset', required = False)
+    parser.add_argument('--save_dir', type=str, default = 'MMBERT/pretrain/save', help='save model weights in this dir', required = False)
+    parser.add_argument('--mlm_prob', type=float, required = True, help='probability of token being masked')
+    parser.add_argument('--mixed_precision', action='store_true', required = False, default = False,  help='mixed precision training or not')
+    parser.add_argument('--resume', action='store_true', required = False, default = False,  help='resume training or train from scratch')
+
+    parser.add_argument('--task', type=str, default='MLM',
+                        choices=['MLM'], help='pretrain task for the model to be trained on')
+    parser.add_argument('--clinicalbert', type=str, default='emilyalsentzer/Bio_ClinicalBERT')
+    parser.add_argument('--max_token_length', type=int, default=512, help='max token length for the transformer in distillation')
+
+    parser.add_argument('--batch_size', type=int, default=16, help='batch_size.')
+    parser.add_argument('--lr', type=float, default=2e-5, help='learning rate')
+    parser.add_argument('--patience', type=int, default=5, help='rlp patience')
+    parser.add_argument('--factor', type=float, default=0.1, help='rlp factor')
+    parser.add_argument('--num_workers', type=int, default=4, help='num works to generate data.')
+    parser.add_argument('--epochs', type=int, default=10, help='epochs to train')
+
+    parser.add_argument('--train_pct', type=float, default=1.0, help='fraction of train set')
+    parser.add_argument('--valid_pct', type=float, default=1.0, help='fraction of validation set')
+    parser.add_argument('--test_pct', type=float, default=1.0, help='fraction of test set ')
+
+    parser.add_argument('--max_position_embeddings', type=int, default=75, help='embedding size')
+    parser.add_argument('--n_layers', type=int, default=4, help='num of heads in multihead attenion')
+    parser.add_argument('--heads', type=int, default=12, help='num of bertlayers')
+    parser.add_argument('--type_vocab_size', type=int, default=2, help='types of tokens ')
+    parser.add_argument('--vocab_size', type=int, default=30522, help='vocabulary size')
+    parser.add_argument('--hidden_size', type=int, default=768, help='embedding size')
+    parser.add_argument('--hidden_dropout_prob', type=float, default=0.3, help='dropout')
+
+    parser.add_argument('--val_loss_resume', type=float, default=np.inf, help='val loss threshold to resume execution')
+    parser.add_argument('--dataset', type=str, default='roco', help='roco or vqamed2019')
+    parser.add_argument('--cnn_encoder', type=str, default='resnet152', help='name of the cnn encoder')
+    parser.add_argument('--transformer_model', type=str, default='transformer',choices=['transformer', 'realformer', 'feedback-transformer'], help='name of the transformer model')
+
+    parser.add_argument('--num_vis', type = int, default=5, help = "num of visual embeddings")
+
+    args = parser.parse_args()
+
+    assert args.dataset in args.data_dir
+    wandb.init(project='medvqa', name = args.run_name, config = args)
+
+    train_data, val_data  = load_mlm_data(args)
+    # No Image: PMC4240561_MA-68-291-g002.jpg
+    train_data = train_data[train_data['name']!='PMC4345544_yjbm_88_1_93_g04.jpg'].reset_index(drop=True)
+    train_data = train_data[train_data['name']!='PMC4240561_MA-68-291-g002.jpg'].reset_index(drop=True) #no image
+    train_data = train_data[train_data['name']!='PMC4093298_jadp-03-059-g02.jpg'].reset_index(drop=True) #no caption
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    model = Model(args)
+    model.to(device)
+    supcon_model = get_supcon_model(model, args)
+    supcon_model.to(device)
+
+    wandb.watch(model, log='all')
+
+    optimizer = optim.Adam(model.parameters(),lr=args.lr)
+    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, patience = args.patience, factor = args.factor, verbose = True)
+    
+    criterion = nn.NLLLoss()
+    supcon_loss = SupConLoss()
+
+    train_tfm = TwoCropTransform(transforms.Compose([
+                                
+                                transforms.Resize(224), #added with profs
+                                transforms.CenterCrop(224), #added with profs
+                                transforms.RandomResizedCrop(224,scale=(0.95,1.05),ratio=(0.95,1.05)),
+                                transforms.RandomRotation(5),
+                                transforms.ColorJitter(brightness=0.05,contrast=0.05,saturation=0.05,hue=0.05),
+                                transforms.ToTensor(), 
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+                                )
+
+    val_tfm = transforms.Compose([
+                                transforms.Resize(224), #added with profs
+                                transforms.CenterCrop(224), #added with profs
+                                transforms.ToTensor(), 
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+    train_path = os.path.join(args.data_dir,'train')
+    val_path = os.path.join(args.data_dir,'validation')
+    test_path = os.path.join(args.data_dir,'test')
+
+    keywords = get_keywords(args)    
+
+    #train dataset using ROCO from SupCon
+    traindataset = ROCO_SupCon(args, train_data, train_tfm, keywords, mode='train')
+    valdataset = ROCO(args, val_data, val_tfm, keywords, mode = 'validation')
+
+    #train loader using half the batch because the other half is from transformations
+    batch_size_supcon = args.batch_size // 2
+    trainloader = DataLoader(traindataset, batch_size = batch_size_supcon, shuffle=True, num_workers = args.num_workers)
+    valloader = DataLoader(valdataset, batch_size = args.batch_size, shuffle=False, num_workers = args.num_workers)
+    
+    scaler = GradScaler()
+
+    if args.resume:
+        ckpt = torch.load(os.path.join(args.save_dir, 'recorder_2.pt'))
+        model.load_state_dict(ckpt['model'])
+        optimizer.load_state_dict(ckpt['optimizer'])
+        scheduler.load_state_dict(ckpt['scheduler'])
+        scaler.load_state_dict(ckpt['scaler'])
+
+    if args.resume:
+        if args.val_loss_resume == np.inf:
+            print('using val loss registered in scheduler')
+            best_loss = scheduler.best
+        else:
+            print('using val loss given as argument')
+            best_loss = args.val_loss_resume
+        print(best_loss)
+        #model.load_state_dict( torch.load(os.path.join(args.save_dir, args.task , args.run_name + '.pt')))
+    else:
+        best_loss = np.inf
+
+    save_recorder = 5
+
+    for epoch in range(args.epochs):
+        
+        print(f'Epoch {epoch+1}/{args.epochs}')
+
+        #train routine from SupCon, regular validation loader, model, supcon_model, criterion, supcon_loss, optimizer, device, args, epoch
+        train_loss, train_acc = train_one_epoch(trainloader, model, supcon_model, criterion, supcon_loss, optimizer, device, args, epoch)
+        val_loss, predictions, acc = validate(valloader, model, criterion, scaler, device, args, epoch)
+
+        scheduler.step(val_loss)
+
+        if (epoch + 1) % save_recorder == 0:
+            recorder = {'epoch': epoch,
+                    'optimizer': optimizer.state_dict(),
+                    'scheduler': scheduler.state_dict(),
+                    'scaler': scaler.state_dict(),
+                    'model': model.state_dict()}
+
+            torch.save(recorder, os.path.join(args.save_dir, 'recorder_2.pt'))
+            
+
+        wandb.log({'epoch_train_loss': train_loss,
+                    'epoch_val_loss': val_loss,
+                    'epoch_train_acc': train_acc,
+                    'epoch_val_acc': acc,
+                    'learning_rate': optimizer.param_groups[0]["lr"],
+                    'epoch': epoch})
+        
+
+        content = f'Learning rate: {(optimizer.param_groups[0]["lr"]):.7f}, Train loss: {(train_loss):.4f}, Train acc: {(train_acc):.4f} ,Val loss: {(val_loss):.4f}, Val acc: {(acc):.4f}'
+        print(content)
+        
+        if val_loss<best_loss:
+            print('Saving model')
+            torch.save(model.state_dict(), os.path.join(args.save_dir, args.task , args.run_name + '.pt'))
+            best_loss=val_loss
